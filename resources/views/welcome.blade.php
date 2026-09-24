ad0582456fa675442dc7b9c4f011919c29cb7fea
firmaaaaan <firmansyah@unisayogya.ac.id>
2026-09-24 12:55:36 +0700

    fix: add favicon to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Welcome - SimKom - UPT Lab Terpadu</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
